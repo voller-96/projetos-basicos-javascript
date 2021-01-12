e318790a53104cee34e104ab2695ebb124ae16ac
Voller Silas <voller.silas@gmail.com>
2021-01-12 13:24:27 -0300

    Lista do Brasileirão

---
+++ 01.js
@@ -0,0 +1,6 @@
+//Lista do campeonato brasileiro com pontuação e time
+let listaBrasileirao;
+listaBrasileirao = [[`São Paulo`,56],[`Internacional`,53],
+                [`Atlético-mg`,50]]
+
+console.log(listaBrasileirao)
